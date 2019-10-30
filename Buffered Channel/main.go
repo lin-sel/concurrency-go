69ff4b7032a8da663f6ef3d476006915a7aa5a53
lin-sel <ccnsy86@gmail.com>
2019-10-30 23:37:58 +0530

    Buffered Channel

---
+++ Buffered Channel/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	ch := make(chan int, 2)
+
+	wg.Add(2)
+
+	go runMain1(ch)
+	go runMain2(ch)
+
+	wg.Wait()
+
+	fmt.Println("Ending Main Function......")
+}
+
+func runMain1(mk chan int) {
+	defer wg.Done()
+	fmt.Println("Adding Value to Channel...")
+	mk <- 10
+}
+
+func runMain2(mk chan int) {
+	defer wg.Done()
+	fmt.Println("Receiving Value From Channel...")
+	fmt.Println(<-mk)
+}
